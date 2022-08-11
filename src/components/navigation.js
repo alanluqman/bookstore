1319c52d099925160104cf7156158fe6550309d5
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-08-11 16:36:35 +0300

    Create navigation bar

---
+++ src/components/navigation.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import './navigation.css';
+
+function Navigation() {
+  return (
+    <>
+      <div className="nav-bar">
+        <nav className="nav-container">
+          <div>
+            <Link className="logo" to="/">Bookstore CMS</Link>
+            <Link className="nav-items" to="/">HOME</Link>
+            <Link className="nav-items" to="/">CATIGORIES</Link>
+          </div>
+          <i className="user fa fa-user" />
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Navigation;

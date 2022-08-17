683d55cf147899d9bb58bec13dcce0ce20c440a1
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-08-17 18:58:25 +0300

    Connect react-redux , implement add, remove button

--- src/components/book.js
+++ src/components/book.js
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 import './book.css';
 
-function Book({ title, author }) {
+function Book({ title, author, id }) {
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>{title}</h1>
       <p>{author}</p>
-      <button type="button">Remove</button>
+      <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
     </div>
   );
 }
